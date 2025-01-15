4418851914391d39b67dc1364ac75ec8fc68a2e2
Siddharthan KV <kvsiddharthan@gmail.com>
2025-01-15 23:16:27 +0530

    square_root.py

--- square_root.py
+++ square_root.py
@@ -1,6 +1,15 @@
-import math
-num = float(input("Enter a number: "))
-if num >= 0:
-    print("Square root:", math.sqrt(num))
+numberr = float(input("Enter a number: "))
+if numberr < 0:
+    print("square root of a negative number cannot be found.")
 else:
-    print("Square root undefined for negative numbers")
+    if numberr == 0:
+        result = 0
+    else:
+        no = numberr / 2
+        while True:
+            nextno = (no + numberr / no) / 2
+            if abs(nextno - no) < 10**-9:
+                result = nextno
+                break
+            no = nextno
+    print("The square root is", result)
